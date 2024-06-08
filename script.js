513152f471a43b8fbd70f2fe420041d7710eac6a
PAWAN KUMAR PATHAK <143309781+pawan709128@users.noreply.github.com>
2024-06-08 08:24:01 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -7,10 +7,15 @@ function appendToDisplay(value) {
   document.getElementById('display').innerText += value;
 }
 
+function backspace() {
+  const display = document.getElementById('display');
+  display.innerText = display.innerText.slice(0, -1);
+}
+
 function calculateResult() {
   const display = document.getElementById('display');
   try {
-      display.innerText = eval(display.innerText);
+      display.innerText = eval(display.innerText.replace('%', '/100'));
   } catch {
       display.innerText = 'Error';
   }
